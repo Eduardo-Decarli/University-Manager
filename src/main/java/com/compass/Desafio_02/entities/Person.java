6f55b8b46831217a4ebad310e9134c52adb26fa8
Victor Pimentel Correia <victorpimentelw@gmail.com>
2024-10-28 11:45:44 -0300

    :sparkles: feat: Person entity

---
+++ src/main/java/com/compass/Desafio_02/entities/Person.java
@@ -0,0 +1,45 @@
+package com.compass.Desafio_02.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+@Entity
+@MappedSuperclass
+public abstract class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
