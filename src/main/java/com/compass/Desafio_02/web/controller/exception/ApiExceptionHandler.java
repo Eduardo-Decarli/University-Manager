8cf5b938d0dcdb67c255ca6c59ef69feb22975c2
Eduardo Decarli <decarliedu@gmail.com>
2024-10-29 11:15:21 -0300

    :sparkles: feat: Create ErrorMessage and ApiExceptionHandler

---
+++ src/main/java/com/compass/Desafio_02/web/controller/exception/ApiExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.compass.Desafio_02.web.controller.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    public ResponseEntity<ErrorMessage>
+}
