f207457737051a7a8f326451367aa45b787ffe9e
João Anísio <joaoanisiognd.ja@gmail.com>
2024-10-29 10:59:20 -0300

    Implementação das classes Registration e Discipline

---
+++ src/main/java/com/compass/Desafio_02/entities/Discipline.java
@@ -0,0 +1,61 @@
+package com.compass.Desafio_02.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Discipline {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "full_professor_id", nullable = false)
+    private Teacher fullProfessor;
+
+    @ManyToOne
+    @JoinColumn(name = "substitute_teacher_id")
+    private Teacher substituteTeacher;
+
+    @ManyToMany
+    @JoinTable(
+            name = "discipline_student",
+            joinColumns = @JoinColumn(name = "discipline_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Discipline that = (Discipline) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
