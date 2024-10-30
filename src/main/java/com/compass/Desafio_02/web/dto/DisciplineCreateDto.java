5c94be7673f487225263fa4bad0abcfede4ba126
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-10-30 10:36:37 -0300

    ✨ feat: discipline create validation annotations added

--- src/main/java/com/compass/Desafio_02/web/dto/DisciplineCreateDto.java
+++ src/main/java/com/compass/Desafio_02/web/dto/DisciplineCreateDto.java
@@ -2,6 +2,7 @@ package com.compass.Desafio_02.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,15 +15,18 @@ public class DisciplineCreateDto {
     @NotBlank(message = "The name can't be empty")
     private String name;
 
+    @Size(min = 5, max = 500, message = "The description must be between 5 and 500 characters")
     private String description;
 
     @NotNull(message = "Full Professor ID is required")
     private Long fullProfessorId;
 
+    @NotNull(message = "Substitute Teacher ID is required")
     private Long substituteTeacherId;
 
     @NotNull(message = "Course ID is required")
     private Long courseId;
 
+    @Size(min = 1, message = "At least one student ID is required")
     private List<Long> studentIds;
 }
