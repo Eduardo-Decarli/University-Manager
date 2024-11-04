09a0ccf8b6e8dd98b9da0d55436025152da714f0
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-11-03 22:46:32 -0300

    ✨ fix: insert statements in sql archive

--- src/test/resources/sql/InsertDataInSQL.sql
+++ src/test/resources/sql/InsertDataInSQL.sql
@@ -1,105 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: localhost    Database: university_system
--- ------------------------------------------------------
--- Server version	8.0.39
+INSERT INTO `coordinator` (`id`, `birth_date`, `email`, `first_name`, `last_name`, `password`, `role`)
+VALUES (1, '2000-01-15', 'joe@example.com', 'Joe', 'Fontes', '$2a$10$zGp1XxUHnH9fsKMJlw8YLe5RGUxqjgMXcW4VYHZxvahgJgRNKVavm', 'ROLE_COORDINATOR');
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+INSERT INTO `course` (`id`, `description`, `name`, `coordinator_id`) VALUES (1,'Course description 1','Computer Science',1);
 
---
--- Dumping data for table `coordinator`
---
+INSERT INTO `discipline` (`id`, `description`, `name`) VALUES (1,'Introduction to Computer Science','CS101');
 
-LOCK TABLES `coordinator` WRITE;
-/*!40000 ALTER TABLE `coordinator` DISABLE KEYS */;
-INSERT INTO `coordinator` VALUES (1,'2000-01-15','joe@example.com','Joe','Fontes','$2a$10$zGp1XxUHnH9fsKMJlw8YLe5RGUxqjgMXcW4VYHZxvahgJgRNKVavm','ROLE_COORDINATOR',NULL,NULL);
-/*!40000 ALTER TABLE `coordinator` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `student` (`id`, `birth_date`, `email`, `first_name`, `last_name`, `password`, `role`, `address`, `course_id`) VALUES (1,'2000-05-15','john@email.com','John','Reis','$2a$10$0X8RBwIqdBWLIJ/jSjF4puDAZuqOVSK2QDOIXNeU8.XoS4fehIRTy','ROLE_STUDENT','Santa Catarina/SC | Palhoça, Passa Vinte, Rua João Cândido da Rosa',1);
 
---
--- Dumping data for table `course`
---
+INSERT INTO `registration` (`id`, `course_id`, `student_id`) VALUES (1,1,1);
 
-LOCK TABLES `course` WRITE;
-/*!40000 ALTER TABLE `course` DISABLE KEYS */;
-INSERT INTO `course` VALUES (1,'Course description 1','Computer Science',1);
-/*!40000 ALTER TABLE `course` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `teacher` (`id`, `birth_date`, `email`, `first_name`, `last_name`, `password`, `role`) VALUES (1,'2000-01-15','doe@example.com','Doe','Dois','$2a$10$mt6JSlRr886RbK6bEgAQU.8.Qs.uv9nPRgaxwKrMtJNkZhZBcaYo6','ROLE_TEACHER');
 
---
--- Dumping data for table `course_disciplines`
---
-
-LOCK TABLES `course_disciplines` WRITE;
-/*!40000 ALTER TABLE `course_disciplines` DISABLE KEYS */;
-/*!40000 ALTER TABLE `course_disciplines` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `discipline`
---
-
-LOCK TABLES `discipline` WRITE;
-/*!40000 ALTER TABLE `discipline` DISABLE KEYS */;
-INSERT INTO `discipline` VALUES (1,'Introduction to Computer Science',NULL,'CS101',NULL);
-/*!40000 ALTER TABLE `discipline` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `discipline_students`
---
-
-LOCK TABLES `discipline_students` WRITE;
-/*!40000 ALTER TABLE `discipline_students` DISABLE KEYS */;
-/*!40000 ALTER TABLE `discipline_students` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `registration`
---
-
-LOCK TABLES `registration` WRITE;
-/*!40000 ALTER TABLE `registration` DISABLE KEYS */;
-INSERT INTO `registration` VALUES (1,1,1);
-/*!40000 ALTER TABLE `registration` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `student`
---
-
-LOCK TABLES `student` WRITE;
-/*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (1,'2000-05-15','john@email.com','John','Reis','$2a$10$0X8RBwIqdBWLIJ/jSjF4puDAZuqOVSK2QDOIXNeU8.XoS4fehIRTy','ROLE_STUDENT','Santa Catarina/SC | Palhoça, Passa Vinte, Rua João Cândido da Rosa',1);
-/*!40000 ALTER TABLE `student` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `teacher`
---
-
-LOCK TABLES `teacher` WRITE;
-/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
-INSERT INTO `teacher` VALUES (1,'2000-01-15','doe@example.com','Doe','Dois','$2a$10$mt6JSlRr886RbK6bEgAQU.8.Qs.uv9nPRgaxwKrMtJNkZhZBcaYo6','ROLE_TEACHER',NULL,NULL,NULL,NULL);
-/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-11-03 21:29:28
