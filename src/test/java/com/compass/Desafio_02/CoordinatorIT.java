575c7768b49173aaeaceaa5d5f4ef6914df5065b
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-11-03 21:37:39 -0300

    ✨ feat: Coordinator test class added

---
+++ src/test/java/com/compass/Desafio_02/CoordinatorIT.java
@@ -0,0 +1,17 @@
+package com.compass.Desafio_02;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/InsertDataInSQL.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/DeleteDataInSQL.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class CoordinatorIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+
+}
